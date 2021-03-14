d5e045d370bddf6261c86c9494555b907d0c04ba
manfred.madelaine <manfred.madelaine@outlook.fr>
2021-03-14 21:10:05 +0100

    :sparkles: FEATURE: Handle treasures

--- back.py
+++ back.py
@@ -1,5 +1,6 @@
-from collections import namedtuple
+import logging as log
 from enum import Enum
+from collections import namedtuple
 
 Cell = namedtuple("Cell", ["i", "j"])
 
@@ -10,13 +11,16 @@ class CellType(Enum):
 
 
 class Map:
-    grid = {}
 
-    def __init__(self, width, height, mountains=[]):
+    def __init__(self, width, height, mountains=[], treasures=[]):
         self.width = width if width >= 0 else 0
         self.height = height if height >= 0 else 0
 
+        self.grid = {}
+        self.treasures = {}
+
         self.create_map(mountains)
+        self.handle_treasures(treasures)
 
     def __str__(self):
         mountain_char = "/\\"
@@ -31,16 +35,29 @@ class Map:
         return "The Madre de Dios Map !" + txt
 
     def create_map(self, mountains):
+        """
+        Creates the map based on the width and height stored in the instance while handling the mountains.
+        :param mountains: List of mountains coordinates which should be present on the map, example [(0, 0), (0, 1)]
+        """
         for i in range(self.width):
             for j in range(self.height):
                 self.grid[Cell(i, j)] = CellType.MOUNTAIN if (i, j) in mountains else CellType.PLAIN
 
+    def handle_treasures(self, treasures):
+        for x, y, treasures_amount in treasures:
+            if (x, y) in self.grid and self.grid[(x, y)] is not CellType.MOUNTAIN:
+                if treasures_amount < 1:
+                    log.debug(f"Trying to set treasures on cell({(x, y)}) but amount is invalid (amount={treasures_amount}).")
+                elif self.treasures.get((x, y)):
+                    log.debug(f"Treasures already set for cell({(x, y)}). Skipping treasure's definition.")
+                else:
+                    self.treasures[(x, y)] = treasures_amount
+
     def get_mountains_count(self):
-        m_count = 0
-        for i in range(self.width):
-            for j in range(self.height):
-                m_count += self.grid[Cell(i, j)] == CellType.MOUNTAIN
-        return m_count
+        return sum([self.grid[Cell(i, j)] == CellType.MOUNTAIN for i in range(self.width) for j in range(self.height)])
+
+    def get_treasures_count(self):
+        return sum([treasures for treasures in self.treasures.values()])
 
 
 if __name__ == "__main__":
