5fd43209efc16f09877a67e684bb201bb5f6b75d
manfred.madelaine <manfred.madelaine@outlook.fr>
2021-03-14 21:10:40 +0100

    :robot: TEST: Treasures creation

--- test_back.py
+++ test_back.py
@@ -6,7 +6,7 @@ from back import Map
 
 class MapTest(unittest.TestCase):
     @parameterized.expand([
-        ["Limit", 0, 0, (0, 0)],
+        ["No map", 0, 0, (0, 0)],
         ["Small map", 1, 1, (1, 1)],
         ["Normal map", 3, 4, (3, 4)],
         ["Big map", 10, 20, (10, 20)],
@@ -26,7 +26,7 @@ class MapTest(unittest.TestCase):
     @parameterized.expand([
         ["Normal map with 1 mountain", 3, 4, [(0, 0)], 1],
         ["Normal map with 2 mountains", 3, 4, [(0, 0), (1, 1)], 2],
-        ["Impossible 2 mountains on same cell", 3, 4, [(1, 1), (1, 1)], 1],
+        ["Ignore mountain creation when on same cell as another mountain", 3, 4, [(1, 1), (1, 1)], 1],
         ["Out of bound mountain in Positives", 3, 4, [(8, 8)], 0],
         ["Out of bound mountain in Positive for one coordinate", 3, 4, [(2, 8)], 0],
         ["Out of bound mountain in Negatives", 3, 4, [(-1, -1)], 0],
@@ -34,6 +34,30 @@ class MapTest(unittest.TestCase):
     def test_map_creation_with_plains_and_mountains(self, name, width, height, mountains, expected):
         quest_map = Map(width, height, mountains)
         assert quest_map.get_mountains_count() == expected
+
+    @parameterized.expand([
+        ["Normal map with 1 treasure", 3, 4, [(0, 0, 1)], 1],
+        ["Normal map with 2 treasures on different cells", 3, 4, [(0, 0, 1), (1, 1, 1)], 2],
+        ["Normal map with 3 treasure in one cell", 3, 4, [(0, 0, 3)], 3],
+        ["Incorrect treasure amount (0)", 3, 4, [(0, 0, 0)], 0],
+        ["Incorrect treasure amount (-3)", 3, 4, [(0, 0, -3)], 0],
+        ["Ignore treasure's duplication (1 new)", 3, 4, [(0, 0, 1), (0, 0, 1)], 1],
+        ["Ignore treasure's duplication (2 news)", 3, 4, [(0, 0, 1), (0, 0, 2), (0, 0, 3)], 1],
+        ["Out of bound treasure in Positives", 3, 4, [(8, 8, 1)], 0],
+        ["Out of bound treasure in Positive for one coordinate", 3, 4, [(2, 8, 2)], 0],
+        ["Out of bound treasure in Negatives", 3, 4, [(-1, -1, 1)], 0],
+    ])
+    def test_map_creation_with_treasures(self, name, width, height, treasures, expected):
+        quest_map = Map(width, height, treasures=treasures)
+        assert quest_map.get_treasures_count() == expected
+
+    @parameterized.expand([
+        ["Normal map with 1 mountain and 1 treasure", 3, 4, [(0, 0)], [(0, 1, 1)], 1],
+        ["Ignore treasure when on same cell as mountain", 3, 4, [(0, 0)], [(0, 0, 1)], 0],
+    ])
+    def test_map_creation_with_treasures_and_mountains(self, name, width, height, mountains, treasures, expected):
+        quest_map = Map(width, height, mountains, treasures)
+        assert quest_map.get_treasures_count() == expected
 
 
 if __name__ == '__main__':
