4d7bd98f535a9e97f1a5629a21d8f629390d6a90
manfred.madelaine <manfred.madelaine@outlook.fr>
2021-03-15 01:16:52 +0100

    :tada: NEW: Players can now move and change directions !

--- player.py
+++ player.py
@@ -2,10 +2,10 @@ from enum import Enum
 
 
 class Direction(Enum):
-    EAST = (0, 1),
-    WEST = (1, 0),
-    NORTH = (-1, 0),
-    SOUTH = (0, -1)
+    NORTH = (0, -1)
+    SOUTH = (0, 1)
+    EAST = (1, 0)
+    WEST = (-1, 0)
 
 
 CHAR_TO_DIRECTION = {
@@ -13,6 +13,7 @@ CHAR_TO_DIRECTION = {
     "S": Direction.SOUTH,
     "E": Direction.EAST,
     "W": Direction.WEST,
+    "O": Direction.WEST,
 }
 
 
@@ -21,11 +22,19 @@ class RelativeDirection(Enum):
     LEFT = -1
 
 
+CHAR_TO_RELATIVE_DIRECTION = {
+    "L": RelativeDirection.LEFT,
+    "G": RelativeDirection.LEFT,
+    "R": RelativeDirection.RIGHT,
+    "D": RelativeDirection.RIGHT,
+}
+
 DIRECTIONS_CLOCKWISE = [Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST]
 
 
 class Player:
-    def __init__(self, pos, direction):
+    def __init__(self, name, pos, direction):
+        self.name = name
         self.pos = pos
         self.direction = CHAR_TO_DIRECTION.get(direction, Direction.NORTH)
 
@@ -33,15 +42,46 @@ class Player:
         return f"{self.pos}, {self.direction.name}"
 
     def move(self):
-        self.pos = self.pos + self.direction
-
-    def turn(self, relative_direction):
         """
-        Allows to turn clockwise or anticlockwise (i.e. to the right or the left).
+        Moves the player in the direction he is facing, i.e. (North, South, East, West).
+        """
+        self.pos = self.get_next_pos()
 
-        :param relative_direction: either left or right
+    def get_next_pos(self):
+        """
+        It simply sums the current position with the direction's value in order to define the player's next position.
+
+        :return: The next position coordinates.
+        """
+        return tuple(map(sum, zip(self.pos, self.direction.value)))
+
+    def turn(self, relative_direction_char):
+        """
+        Turns the player clockwise or anticlockwise (i.e. to the right or to the left).
+
+        :param relative_direction_char: either left or right ("L" or "R")
         :return: new direction (North, South, East, West).
         """
+        relative_direction = CHAR_TO_RELATIVE_DIRECTION.get(relative_direction_char)
+        if not relative_direction:
+            return
+
         dir_index = DIRECTIONS_CLOCKWISE.index(self.direction)
-        new_dir_index = (dir_index + relative_direction) % len(DIRECTIONS_CLOCKWISE)
-        return DIRECTIONS_CLOCKWISE[new_dir_index]
+        new_dir_index = (dir_index + relative_direction.value) % len(DIRECTIONS_CLOCKWISE)
+        self.direction = DIRECTIONS_CLOCKWISE[new_dir_index]
+
+
+if __name__ == "__main__":
+    p = (0, 0)
+    lara = Player("Lara", p, "E")
+    print(lara)
+
+    actions = "ADAAGAGA"
+    for a in actions:
+        if a == "A":
+            print(f"{lara.name} moves to the {lara.direction.name}.")
+            lara.move()
+            print(lara)  # (2, 1)
+        else:
+            print(f"{lara.name} turns to the {CHAR_TO_RELATIVE_DIRECTION.get(a).name}")
+            lara.turn(a)
--- test_player.py
+++ test_player.py
@@ -6,14 +6,56 @@ from player import Player
 
 class PlayerTest(unittest.TestCase):
     @parameterized.expand([
-        ["Simple NORTH", (0, 0), "N", "(0, 0), NORTH"],
-        ["Simple SOUTH", (0, 0), "S", "(0, 0), SOUTH"],
-        ["Simple EAST", (0, 0), "E", "(0, 0), EAST"],
-        ["Simple WEST", (0, 0), "W", "(0, 0), WEST"],
+        ["Player facing NORTH", (0, 0), "N", "(0, 0), NORTH"],
+        ["Player facing SOUTH", (0, 0), "S", "(0, 0), SOUTH"],
+        ["Player facing EAST", (0, 0), "E", "(0, 0), EAST"],
+        ["Player facing WEST", (0, 0), "W", "(0, 0), WEST"],
+        ["Player facing WEST (FR)", (0, 0), "O", "(0, 0), WEST"],
         ["Incorrect Direction", (0, 0), "Q", "(0, 0), NORTH"],
     ])
     def test_player_creation(self, name, pos, direction, expected):
-        p = Player(pos, direction)
+        p = Player("Lara", pos, direction)
+        assert str(p) == expected
+
+    @parameterized.expand([
+        ["Simple step to the NORTH", (0, 2), "N", "(0, 1), NORTH"],
+        ["Simple step to the SOUTH", (0, 1), "S", "(0, 2), SOUTH"],
+        ["Simple step to the EAST", (1, 0), "E", "(2, 0), EAST"],
+        ["Simple step to the WEST", (2, 0), "W", "(1, 0), WEST"],
+    ])
+    def test_player_movements(self, name, pos, direction, expected):
+        p = Player("Lara", pos, direction)
+        p.move()
+        assert str(p) == expected
+
+    @parameterized.expand([
+        ["Change on the Right when facing NORTH", (1, 0), "N", "R", "(1, 0), EAST"],
+        ["Change on the Left when facing NORTH", (1, 0), "N", "L", "(1, 0), WEST"],
+        ["Change on the Right when facing SOUTH", (1, 0), "S", "R", "(1, 0), WEST"],
+        ["Change on the Left when facing SOUTH", (1, 0), "S", "L", "(1, 0), EAST"],
+        ["Change on the Right when facing EAST", (1, 0), "E", "R", "(1, 0), SOUTH"],
+        ["Change on the Left when facing EAST", (1, 0), "E", "L", "(1, 0), NORTH"],
+        ["Change on the Right when facing WEST", (1, 0), "W", "R", "(1, 0), NORTH"],
+        ["Change on the Left when facing WEST", (1, 0), "W", "L", "(1, 0), SOUTH"],
+        ["Keep direction unchanged when incorrect orientation", (1, 0), "S", "Q", "(1, 0), SOUTH"],
+    ])
+    def test_player_orientation_changes(self, name, pos, direction, orientation_change, expected):
+        p = Player("Lara", pos, direction)
+        p.turn(orientation_change)
+        assert str(p) == expected
+
+    @parameterized.expand([
+        ["Face NORTH, Move, Turn Right and Move again", (0, 1), "N", "R", "(1, 0), EAST"],
+        ["Face EAST, Move, Turn Right and Move again", (0, 0), "E", "R", "(1, 1), SOUTH"],
+        ["Face SOUTH, Move, Turn Right and Move again", (1, 0), "S", "R", "(0, 1), WEST"],
+        ["Face WEST, Move, Turn Right (FR) and Move again", (1, 1), "W", "D", "(0, 0), NORTH"],
+        ["Face WEST, Move, Turn Left (FR) and Move again", (1, 1), "W", "G", "(0, 2), SOUTH"],
+    ])
+    def test_player_orientation_changes_and_movements(self, name, pos, direction, orientation_change, expected):
+        p = Player("Lara", pos, direction)
+        p.move()
+        p.turn(orientation_change)
+        p.move()
         assert str(p) == expected
 
 
